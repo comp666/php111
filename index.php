e8eca93e865886cf83e77b80f8b58428d988d090
comp666 <comp622012016@mail.ru>
2016-09-08 17:37:40 +0300

    first commit

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <meta charset="utf-8">
 <?php
 $example=33;
-echo $example;
-echo "</br>";
-if(is_integer($example));
-echo "yes";
+  echo $example;
+  echo "<br />";
+  
+if(is_integer($example))
+{
+  echo "yes";
+  echo "<br />";
+  }
+if(isset($example))
+{
+  echo "yes";
+  echo "<br />";
+}
+if(is_double($example))
+{
+  echo "yes";
+  echo "<br />";
+}
+$taptap="тест";
+  echo $taptap;
+  echo "<br />";
+  if (isset($taptap)){
+	  echo "yes";
+  }
+  else {
+	  echo "<br />";
+	  echo "no";
+       }
+	   define ("PI","3,14",true);
+	   echo PI;
 
 ?>
